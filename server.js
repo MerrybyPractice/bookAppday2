f960c41492e6a2c45723675ed609e2e4dae63076
Merry Cimakasky <mkcimakasky@gmail.com>
2019-03-27 11:05:58 -0700

    some work towards server up, still failing

--- server.js
+++ server.js
@@ -28,12 +28,12 @@ app.get('*', (request, response) => response.status(404).send('Get the HELL out 
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
 
 //Helper
-let pathToBook = request.body.items.volumeInfo
-function book(info) {
-  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
 
-  this.title = info.title || 'No Title Available';
+client.on('error', err => console.error(err))
 
+function handleError(err, result){
+  console.error(err);
+  if (res) res.status(500).send('So very sorry, something went WRONG.');
 }
 
 //Note that ejs file is not required
@@ -44,20 +44,39 @@ function newSearch(request, response) {
 //No API key required
 //console.log request.body and request.body.search
 function createSearch(request, response) {
-  let url = 'https:///www.googleapis.com/books/v1/volumes?q=';
+  let url = `https:///www.googleapis.com/books/v1/volumes?q=in${request.body.search[1]}:${request.body.search[0]}`;
 
   console.log(request.body);
 
   if (request.body.search[1] === 'title') {url += `+intitle:${request.body.search[0]}`;}
   if (request.body.search[1] === 'author') {url += `+inauthor:${request.body.seach[0]}`;}
-
   console.log(url);
 
   superagent.get(url)
-    .then(apiResponse => apiResponse.body.items.volumeInfo.map(bookResult => new Book(bookResult.volumeInfo)))
-    function book((info) => {
-      
-    }
-    ])
-    .then(results => response.render('pages/searches/show', {searchResults: results}));
+    .then(results => {
+      if (results.body.totalItems === 0) { handleError({status:404}, response);
+      }else{
+        let bookArray = results.body.items.map((bookData) => {
+
+          let book = new Book(bookData.volumeInfo);
+          return book;
+        });
+      }
+    })
+    .then(results => response.render('pages/searches/show', { searchResults: results }));
+    .catch(err => (handleError(err, response));
+  }
+
+//book constructor
+
+let pathToBook = response.body.items.volumeInfo
+
+function Book(bookArray) {
+
+  this.title = pathToBook.title || 'No Title Available';
+  this.img = pathToBook.imageLinks.thumbnail || 'https://i.imgur.com/J5LVHEL.jpg';
+  this.description = pathToBook.description || 'No one felt the need to describe this. How sad.';
+  this.authors = pathToBook.authors || 'There is no one who takes credit for this work.';
+  this.isbn10 = pathToBook.industryIdentifiers[0].identifiers || 'This book was published before 2007 and no one has wanted to republish it after.This book was published after 2007!';
+  this.isbn13 = pathToBook.industryIdentifiers[1].identifiers || 'This book was published after 2007. Has anything good happened after 2007, really?';
 }
